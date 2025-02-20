6cf51b9ffa687b527f06e8abb8307642f8cbe03f
LJS7628 <dlwotmd7628@naver.com>
2025-02-21 03:25:54 +0900

    .
    .

--- MyPortfolio/Intermediate/Build/BuildRulesProjects/MyPortfolioModuleRules/obj/Development/MyPortfolioModuleRules.AssemblyInfo.cs
+++ MyPortfolio/Intermediate/Build/BuildRulesProjects/MyPortfolioModuleRules/obj/Development/MyPortfolioModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolioModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5693c7410e7a139e2b58213771c4ec5ccdb5bc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b10481b513781091660c17fb7745ef32f88b674")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolioModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolioModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
